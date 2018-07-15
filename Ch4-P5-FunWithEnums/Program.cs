aa8998b36b1e6381dcd279676d94df38724b04a4
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-15 20:51:19 +0500

    first project commit from KQ Hostel

---
+++ Ch4-P5-FunWithEnums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_P5_FunWithEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chapter 4 - Project 5 - Enums in C# ");
+            Console.ReadLine();
+
+        }
+    }
+}
